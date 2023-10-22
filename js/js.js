79f27dc4f9c049cdbae7d973c071d6208370a504
Silvana <galerasilgab@gmail.com>
2023-10-21 23:37:55 -0300

    validacion en js de formulario contacto

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var formulario = document.getElementById("cto_frm");
   
     var validarNombre = function (e) {
-      if (formulario.nombrecompleto.value.length < 10) {
+      if (formulario.nombrecompleto.value.length < 2) {
         alert("Por favor, escribe tu nombre y apellido.");
         document.getElementById("nombre").focus();
         return false;
@@ -10,11 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
       return true;
     };
 
+    var validarEdad = function () {
+        if (formulario.edad.value.length == 0) {
+          alert("Por favor, ingrese edad.");
+          document.getElementById("edad").focus();
+          return false;
+        }
+        return true;
+    };
+
+    var validarFnac = function () {
+        if (formulario.fn.value.length == 0) {
+          alert("Por favor, ingrese una fecha de nacimiento.");
+          document.getElementById("fn").focus();
+          return false;
+        }
+        return true;
+    };
+
     var validarContacto = function (e) {
-        var contacto = document.getElementById("codigo").value;
-        if (isNaN(contacto)) {
-          alert("Por favor, escribe solo números.");
-          document.getElementById("codigo").focus();
+        var cont = parseInt(document.getElementById("contacto").value);
+        if (isNaN(cont)) {
+          alert("Por favor, colocar un numero telefónico válido.");
+          document.getElementById("contacto").focus();
           return false;
         }
         return true;
@@ -22,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       var validarCorreo = function (e) {
         var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
-        var correo = document.getElementById("email").value;
+        var correo = document.getElementById("correo").value;
         if (!expresion.test(correo)) {
           alert("Por favor, escribe un email válido.");
           document.getElementById("correo").focus();
@@ -31,8 +49,18 @@ document.addEventListener("DOMContentLoaded", function () {
         return true;
       };
 
+      var validarOpcion = function () {
+        if (formulario.opcion.value.length == 0) {
+          alert("Por favor, escribe alguna opcion.");
+          document.getElementById("opcion").focus();
+          return false;
+        }
+        return true;
+      };
+
       var validarMensaje = function () {
-        if (formulario.comentario.value.length == 0) {
+        let str = document.getElementById("comentario");
+        if (str.value.trim() == 0) {
           alert("Por favor, escribe algun comentario.");
           document.getElementById("comentario").focus();
           return false;
@@ -42,12 +70,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
       var validar = function () {
         return (
-          validarContacto() &&
-          validarNombre() &&
-          validarCorreo() &&
-          validarMensaje()
+            validarNombre() &&
+            validarEdad() &&
+            validarFnac() &&
+            validarContacto() &&
+            validarCorreo() &&
+            validarOpcion() &&
+            validarMensaje()
         );
       };
+
       formulario.addEventListener("submit", handleSubmit);
       async function handleSubmit(event) {
         event.preventDefault();
